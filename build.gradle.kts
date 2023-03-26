860b8c6e723a1e7d418d5ccb6492ad72ca627af0
jrucl2d <ysk789465@gmail.com>
2023-03-27 02:13:10 +0900

    [COPOR-2] KotlinLogging 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,15 @@ subprojects {
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("io.github.microutils:kotlin-logging:2.0.10")
         testImplementation("io.mockk:mockk:1.13.2")
+        testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.5")
+        testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+        testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.5")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
     }
 }
 
