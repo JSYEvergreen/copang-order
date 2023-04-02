0cd55fd4550a7f1da1c384d8a6a5306af0268bb5
jrucl2d <ysk789465@gmail.com>
2023-04-02 20:14:19 +0900

    [COPOR-2] auth 서버 mock controller 추가

---
+++ copang-order-api/src/main/kotlin/com/copang/mock/MockAuthServerController.kt
@@ -0,0 +1,31 @@
+package com.copang.mock
+
+import com.copang.auth.UserInfo
+import com.copang.web.ApiResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+class MockAuthServerController {
+    @GetMapping("/buyer/login")
+    fun login(
+        request: HttpServletRequest,
+    ): ApiResponse<UserInfo> {
+        val authorizationHeader: String? = request.getHeader(HttpHeaders.AUTHORIZATION)
+        if (authorizationHeader.isNullOrBlank()) {
+            return ApiResponse.fail(
+                errorCode = "10001",
+                message = "올바른 토큰이 아닙니다.",
+            )
+        }
+        return ApiResponse.success(
+            content = UserInfo(
+                id = 1L,
+                userId = "testUserId",
+            )
+        )
+    }
+}
