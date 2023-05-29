09ce02b21afae661ebf10a3a9edfb6c84f0ffc91
jrucl2d <ysk789465@gmail.com>
2023-05-29 14:14:14 +0900

    [COPRO-0] kover, ktlint 의존성 세팅 추가, github workflow 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 plugins {
-    id("org.springframework.boot") version "2.7.5" apply false
+    id("org.springframework.boot") version "2.7.10" apply false
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21" apply false
     kotlin("plugin.jpa") version "1.6.21" apply false
 }
+
+
 allprojects {
     group = "com.example"
     version = "0.0.1-SNAPSHOT"
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "kover")
 
     repositories {
         mavenCentral()
@@ -31,7 +38,7 @@ subprojects {
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        implementation("io.github.microutils:kotlin-logging:2.0.10")
+        implementation("io.github.microutils:kotlin-logging:2.1.23")
         testImplementation("io.mockk:mockk:1.13.2")
         testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.5")
         testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
@@ -46,7 +53,6 @@ subprojects {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
     }
 }
 
